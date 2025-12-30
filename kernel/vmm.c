c2dc4403a8ae17a27e7e8bc3604abb6aa814efda
GaoTimmy <2424024592@qq.com>
2025-12-30 07:40:37 +0000

    my work on lab2_1 is done.

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -159,8 +159,16 @@ void *user_va_to_pa(pagetable_t page_dir, void *va) {
   // (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
   // Also, it is possible that "va" is not mapped at all. in such case, we can find
   // invalid PTE, and should return NULL.
-  panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
+  //panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
 
+  //查找va对应物理页地址即PTE
+  uint64 phy_addr = lookup_pa(page_dir, (uint64)va);
+
+  //invalid PTE, and shoule return NULL
+  if (! phy_addr) return NULL;
+
+  //PTE(PPN) is the starting address
+  else return (void *)(phy_addr + ((uint64)va & ((1<<PGSHIFT) - 1)));
 }
 
 //
